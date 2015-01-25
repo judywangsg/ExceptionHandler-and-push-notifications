1f1fbfcef673772153ef2e245f136e2a4bcf26c2
PALANIAPPAN N <paln@cisco.com>
2015-01-26 00:29:26 +0530

    fixes

--- Python lib/ExceptionHandler.py
+++ Python lib/ExceptionHandler.py
@@ -2,44 +2,82 @@ import traceback
 import requests
 import httplib, urllib
 from time import strftime
-reg_id = "APA91bFQlavXTnIFW9TRB0RNszYfkFFg-D4ePT6T0dV4nJfWHwubGP5K4oGPZnK8hMkBRQx15VK0QTlPONylKaS3Vdp0Jhf0B0SP0iomf0Ty8EjAHMbkfa03-3XjhmM3irzrTsBOd1iT20AK2JAwGgONPC9VWcrytuKsihlqJMN0Y5TXgbZ1m2E"
+import json
 
-cloudMsgServer = "http://54.149.149.26:5000/sendMsg"
 
-def myExceptionHandler(type, value, tback):
-	print "*" * 50
+class Handler(object):
+	"""
+	This class handles all the uncaught exceptions in the main program. Also provides means to send notification to your Android phone.
 
-	print str(type)
-	print str(value)
-	print str(tback)
-	print ''.join(traceback.format_tb(tback))
-	print "*" * 50
-	#sendCloudMessage(str(value), str(type) + ''.join(traceback.format_tb(tback)), "ERROR")
-	getStackoverflowQuestion(str(type))
+	"""
 
-def getStackoverflowQuestion(query):
-	print query
-	requestURL = "https://api.stackexchange.com/2.2/search/advanced"
-	data = {
-		'key' : 'U4DMV*8nvpm3EOpvf69Rxw((',
-		'site' : 'stackoverflow',
-		'order' : 'desc',
-		'sort' : 'relevance',
-		'q' : query
-		}
-	r = requests.get(requestURL , data=data, allow_redirects=True)
-	print r.content
+	reg_id = ""
+
+	TAG_ERROR = "ERROR"
+	TAG_INFO = "INFO"
+	TAG_WARNING = "WARNING"
+
+
+	cloudMsgServer = "http://54.149.149.26:5000/sendMsg"
+
+	def __init__(self):
+		self.sendNotificationOnException = True
+		self.searchStackoverflowOnException = False
+		
+	def myExceptionHandler(self, type, value, tback):
+		"""This method catches all the uncaught exceptions in the program. """
+		print "*" * 50
+		print str(type)
+		print str(value)
+		print str(tback)
+		print ''.join(traceback.format_tb(tback))
+		print "*" * 50
+		
+		if(self.sendNotificationOnException):
+			Handler.sendCloudMessage(str(value), str(type) + ''.join(traceback.format_tb(tback)), Handler.TAG_ERROR)
+		
+		if(self.searchStackoverflowOnException):
+			getStackoverflowQuestion(str(type))
+
+	def getStackoverflowQuestion(query):
+		"""Searches stackoverflow for the given query """
+		print query
+		requestURL = "https://api.stackexchange.com/2.2/search/advanced"
+		data = {
+			'key' : 'U4DMV*8nvpm3EOpvf69Rxw((',
+			'site' : 'stackoverflow',
+			'order' : 'desc',
+			'sort' : 'relevance',
+			'q' : query
+			}
+		r = requests.get(requestURL , data=data, allow_redirects=True)
+		print r.content
 	
-def sendCloudMessage(title, msg, type):
 	
-	data = {
-		'title':title,
-		'time': strftime("%Y-%m-%d %H:%M:%S"),
-		'msg' : msg, 
-		'type' : type, 
-		'id' : reg_id
-		}
 
-	r = requests.post(cloudMsgServer , data=data, allow_redirects=True)
-	print r.content
+	@staticmethod
+	def sendCloudMessage(title, msg, type):
+		""" Send notification to your Android phone. This requires installing app """
+		if Handler.reg_id is "":
+			print "Please set your cloud id which is obtained from android app to send notification to your phone"
+			return False
+		else:
+			data = {	
+				'title':title,
+				'time': strftime("%Y-%m-%d %H:%M:%S"),
+				'msg' : msg, 
+				'type' : type, 
+				'id' : Handler.reg_id
+				}
 	
+			r = requests.post(Handler.cloudMsgServer , data=data, allow_redirects=True)
+			#print r.content
+			response = json.loads(r.content)
+
+			if response["success"] > 0:
+				print "Message delivered successfully"
+				return True
+
+			else:
+				print "Fatal : Unable to send message"
+				return False
--- Python lib/Sample.py
+++ Python lib/Sample.py
@@ -1,7 +1,20 @@
 import sys
-from ExceptionHandler import myExceptionHandler
+from ExceptionHandler import Handler
+import ExceptionHandler
 
+""" This is a test program to demonstrate how to use Exception Handle module"""
 
 if __name__ == '__main__':
-    sys.excepthook = myExceptionHandler
+    handler = Handler()
+    sys.excepthook = handler.myExceptionHandler
+    
+    #Registration id obtained from android app goes here..
+    Handler.reg_id = "My registration id"
+    
+    whenSomeonePaysForWinRAR = True
+    if(whenSomeonePaysForWinRAR):
+	    Handler.sendCloudMessage("Someone pays for WinRAR!!", "Hooo Yaaaaayyyy!!!!", Handler.TAG_INFO)
+    
+    #causing runtime exception
     print 4/0
+    
